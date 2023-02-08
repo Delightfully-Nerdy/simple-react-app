038b6b38b88ba5e58d558cd11b4423a0eda79d63
Dalitso Kasonde <dkasonde11@gmail.com>
2023-02-08 14:28:29 +0200

    states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React,{useState} from 'react';
 import './App.css';
 
 // import component 
-import Nav from './components/Nav';
-import Tweets from './components/Tweets';
+
 
 
 function App() {
 
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+
+  const incrementor = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+  const toggler = () => {
+    setToggle((prev) => !prev );
+  }
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <div className="home">
-        <Nav />
-        <Tweets />
-      </div>
+      <h1 className={toggle ? 'active' : ''}>Hello React</h1>
+      <h2>Counter {counter}</h2>
+      <button onClick={incrementor}>Click</button>
+      <button onClick={toggler}>Toggle</button>
     </div>
   );
 }
